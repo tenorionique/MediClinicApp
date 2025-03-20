60f40e4c19f3a2d276d99a7920894ef00399b89f
monique doretto <monique.sa@raizen.com>
2025-03-20 17:45:02 -0300

    adiciona validação de id em appointment

--- models/Appointment.js
+++ models/Appointment.js
@@ -3,23 +3,31 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const appointmentSchema = new Schema({
-  date: {
-    type: Date,
-    required: true
-  },
+  date: { type: Date, required: true },
   doctorId: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: async function (v) {
+        if (!mongoose.Types.ObjectId.isValid(v)) return false;
+        return await Doctor.exists({ _id: new mongoose.Types.ObjectId(v) });
+      },
+      message: props => `DoctorId ${props.value} not found.`
+    }
   },
   pacientId: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: async function (v) {
+        if (!mongoose.Types.ObjectId.isValid(v)) return false;
+        return await Pacient.exists({ _id: new mongoose.Types.ObjectId(v) });
+      },
+      message: props => `PacientId ${props.value} not found.`
+    }
   },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-})
+  createdAt: { type: Date, default: Date.now }
+});
 
-const appointment = mongoose.model("Appointment", appointmentSchema);
-export default appointment;
+const Appointment = mongoose.model("Appointment", appointmentSchema);
+export default Appointment;
